8f95cb922c616a237c355e5cb25d5419424dc9c2
Ky Porter <kylamporter@gmail.com>
2018-02-01 08:54:26 -0800

    pretesting finalized makeDB

--- makeMediaDB.py
+++ makeMediaDB.py
@@ -4,9 +4,11 @@ genres = {'Movie': [('Horror',), ('Comedy',), ('Action',), ('Drama',), ('Science
 
 formats = {'Movie': [('4K',), ('DVD',), ('VHS',)], 'Written': [('eBook',), ('Paperback',), ('Magazine',)], 'Music': [('mp4',), ('CD',), ('Record',)]}
 
+
 Class MediaDB:
 
-	def __init__(self, connectionName='testmoviedb.db', mediaType='movie'):
+	def __init__(self, owner, connectionName='testmoviedb.db', mediaType='movie'):
+		self.deity = owner
 		self.mType = mediaType
 		#start connection
 		self.conn = s3.connect(connectionName)
@@ -14,6 +16,7 @@ Class MediaDB:
 		self.c = self.conn.cursor()
 		self.makeTables
 		self.populateInfo
+		self.conn.close()
 
 	def makeTables(self):
 		#create tables:	
@@ -23,19 +26,20 @@ Class MediaDB:
 			conn.commit()
 
 		#Items
-		c.execute("CREATE TABLE Items(m_id INTEGER PRIMARY KEY, title text, favorite integer DEFAULT 0, unused integer, fs_id integer DEFAULT 0, FOREIGN KEY(fs_id) REFERENCES Series(s_id))")
+		c.execute("CREATE TABLE Items(i_id INTEGER PRIMARY KEY, title text, favorite integer DEFAULT 0, unused integer, fs_id integer DEFAULT 0, FOREIGN KEY(fs_id) REFERENCES Series(s_id))")
 		conn.commit()
 
 		if self.mType != 'Movie':
-			c.execute("ALTER TABLE Items ADD COLUMN author {text} DEFAULT {'Anonymous'}")
+			c.execute("CREATE TABLE Authors(a_id INTEGER PRIMARY KEY, name text")
+			c.execute("ALTER TABLE Items ADD COLUMN fa_id {integer} DEFAULT {0}, FOREIGN KEY(fa_id) REFERENECES Authors(a_id)")
 			conn.commit()
 
 		#Genres
 		c.execute("CREATE TABLE Genres(g_id INTEGER PRIMARY KEY, genre_name text UNIQUE)")
 		conn.commit()
 
-		#Movie_is_a
-		c.execute("CREATE TABLE Movie_is_a(fm_id integer, fg_id integer, FOREIGN KEY(fm_id) REFERENCES Movies(m_id), FOREIGN KEY(fg_id) REFERENCES Genres(g_id))")
+		#Item_is_a
+		c.execute("CREATE TABLE Item_is_a(fi_id integer, fg_id integer, FOREIGN KEY(fi_id) REFERENCES Items(i_id), FOREIGN KEY(fg_id) REFERENCES Genres(g_id))")
 		conn.commit()
 
 		#Formats
@@ -43,7 +47,7 @@ Class MediaDB:
 		conn.commit()
 
 		#Movie_on_a
-		c.execute("CREATE TABLE Movie_on_a(fm_id integer, ff_id integer, FOREIGN KEY(fm_id) REFERENCES Movies(m_id), FOREIGN KEY(ff_id) REFERENCES Formats(f_id))")
+		c.execute("CREATE TABLE Item_on_a(fi_id integer, ff_id integer, FOREIGN KEY(fi_id) REFERENCES Items(m_id), FOREIGN KEY(ff_id) REFERENCES Formats(f_id))")
 		conn.commit()
 
 	def populateInfo(self):
@@ -57,5 +61,3 @@ Class MediaDB:
 		formatList = formats[self.mType]
 		c.executemany("INSERT INTO Formats(format_name) Values (?)", formatList)
 		conn.commit()
-
-		conn.close()
