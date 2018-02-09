c49be22f9841dc9b46b9c2976155774556e5efd3
Ky Porter <kylamporter@gmail.com>
2018-02-09 11:44:26 -0800

    finished get info window

--- runMediaDB.py
+++ runMediaDB.py
@@ -2,43 +2,26 @@ from makeMediaDB import *
 from guiFunctions import *
 from tkinter import *
 from tkinter import ttk
+import time
 
 #build config
 
 #read data from config
 config = "mdConfig.txt"
 
-class TypeButtons(ttk.Frame):
-	def __init__(self, parent):
-		super().__init__(parent)
-		self.columnconfigure(0, weight=1)
-		self.columnconfigure(1, weight=1)
-		self.columnconfigure(2, weight=1)
-		self.rowconfigure(0, weight=1)
-		self.rowconfigure(1, weight=1)
-		self.rowconfigure(2, weight=1)
-		self.typelab = ttk.Label(self, text="Select Media Type:")
-		self.typevar = IntVar()
-		self.bookbutton = ttk.Radiobutton(self, text="Book", variable=self.typevar, value=1)
-		self.moviebutton = ttk.Radiobutton(self, text="Movie", variable=self.typevar, value=2)
-		self.musicbutton = ttk.Radiobutton(self, text="Music", variable=self.typevar, value=3)
-		self.confbutton = ttk.Button(self, text="Create New Database", command=self.sendInfo)
-		self.typelab.grid(column = 0, row=0, columnspan=3, sticky=N+W+S+E)
-
-	def sendInfo(self):
+medDict = {1: "book", 2: "movie", 3: "music"}
 		
-		
-
 class GetInfoWindow(ttk.Frame):
-	def __init__(self,master=None):
+	def __init__(self,infodict,master=None):
 		super().__init__(master)
 		self.columnconfigure(0, weight=1)
 		self.columnconfigure(1, weight=1)
 		self.columnconfigure(2, weight=1)
-		self.rowconfigure(0, weight=1)
-		self.rowconfigure(1, weight=1)
-		self.rowconfigure(2, weight=1)		
-
+		self.rowconfigure(0, weight=1) #name
+		self.rowconfigure(1, weight=1) #radiobutton label
+		self.rowconfigure(2, weight=1) #radiobuttons		
+		self.rowconfigure(3, weight=1) #confirmation button
+		self.dict = infodict
 		self.namelab = ttk.Label(self, text="Username:")
 		self.namevar = StringVar()
 		self.namebox = ttk.Entry(self, textvariable=self.namevar, style='InfoFrame.TLabel')
@@ -48,14 +31,16 @@ class GetInfoWindow(ttk.Frame):
 		self.moviebutton = ttk.Radiobutton(self, text="Movie", variable=self.typevar, value=2)
 		self.musicbutton = ttk.Radiobutton(self, text="Music", variable=self.typevar, value=3)
 		self.confbutton = ttk.Button(self, text="Create New Database", command=self.sendInfo)
-		self.typelab.grid(column = 0, row=0, columnspan=3, sticky=N+W+S+E)
+		self.placeItems()
+		#self.mainloop()
 
 	def sendInfo(self):
-		
+		self.dict['owner'] = self.namevar
+		self.dict['medtype'] = medDict[self.typevar]
+		self.destroy
 
-		self.rowconfigure(0, weight=1)
-		self.rowconfigure(1, weight=1)
-		self.placeItems()
+
+
 
 def __main__():
 	infoDict = {}
@@ -65,19 +50,18 @@ def __main__():
 			if len(splitLine) == 2:
 				infoDict[splitLine[0].lower()] = splitLine[1].lower()
 	
-	if infoDict['owner'] == 'none':
-		infoDict['owner'] = requestOwner()
+	if infoDict['owner'] == 'none' || infoDict['dbname'] == 'none' || infoDict['medtype'] == 'none':
+		infoWnd = GetInfoWindow(infoDict)
+		while infoWnd != None:
+			time.sleep(.1)
+		infoDict['dbname'] = infoDict['owner'].upper() + infoDict['medtype'].lower() + ".db"
 
-	if infoDict['dbname'] == 'none':
-		infoDict['dbname'] = getDbName()
-
-	if infoDict['medtype'] == 'none':
-		infoDict['medtype'] = getType()
 	
 	#errorcheck dupe dbnames
 	currentDB = MediaDB(infoDict['owner'], connectionName=infoDict['dbname'], mediaType=infoDict['medtype'])
 
 	newInstance = AppManager(currentDB)
+	newInstance.makeMainPage()
 
 
 
